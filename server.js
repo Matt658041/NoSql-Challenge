eee12072dbd4b8b03bf900b44e1790fe4ca5a1f0
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-06-06 13:54:51 -0500

    updated serverjs

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/thoughts', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/userdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
