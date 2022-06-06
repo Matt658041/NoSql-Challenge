867496683813e29d14a135ec680824c411ccf964
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-06-06 12:04:24 -0500

    created user models

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = newSchema (
+{
+      userName: {
+          type: String,
+          required: true,
+          trim: true,
+          unique: true
+      },
+      email: {
+          type: String,
+          required:[ true, "Email required"],
+          unique: true,
+          lowercase: true,
+          validate: {
+            validator: function(v) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: "Please enter a valid email"
+      }
+     },
+     thoughts: [
+         {
+             type: Schema.Types.ObjectId,
+             ref: 'Thought'
+         }
+     ]
+},
+{
+    toJson: {
+        virtuals: true,
+        getters: true
+    },
+    //prevents virtuals from creating duplicates
+    id: false
+}
+);
+
+//get count of thougths and reaction on retreival
+UserSchema.vitual('friendCount').get(function() {
+    return this.friends.reduce(
+       (total, friend) => total + friend.replies.length +1,
+       0
+    );
+});
+
+const User = model('user', UserSchema);
+
+module.exports = User;
