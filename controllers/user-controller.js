5048f3b011b7b0172c69cb1f6b09011cacc6edf4
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-06-07 10:41:36 -0500

    npm is working with insomnia but getting validation error for username

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,10 +4,7 @@ const userController = {
   // get all users
   getAllUsers(req, res) {
     User.find({})
-      .populate({
-        path: 'thoughts',
-        select: '-__v'
-      })
+      
       .select('-__v')
       .sort({ _id: -1 })
       .then(dbUserData => res.json(dbUserData))
@@ -24,7 +21,11 @@ const userController = {
         path: 'thoughts',
         select: '-__v'
       })
-      .select('-__v')
+      .populate ({
+        path:"friends",
+        select: '-__v'
+      })
+      
       .then(dbUserData => res.json(dbUserData))
       .catch(err => {
         console.log(err);
