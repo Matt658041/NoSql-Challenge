5c28fa4ef825a729278c7e7ad23e3ae93ba3f8de
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-06-08 10:52:27 -0500

    had to change routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const thoughtRoutes = require('./thought-routes');
+const userRoutes = require('./user-routes');
 
 
-router.use('/api', apiRoutes);
-
-
-router.use((req, res) => {
-  res.status(404).send('<h1>😝 404 Error!</h1>');
-});
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
